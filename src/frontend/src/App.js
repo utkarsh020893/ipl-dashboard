cae72c55a9386bd997fd447fc6a0c99265745ab7
Utkarsh Chauhan <utkarshchauhan020893@gmail.com>
2022-03-07 23:55:11 +0530

    Added

---
+++ src/frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+        <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
+        <Route path="/teams/:teamName" element={<TeamPage />} />
+        <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
